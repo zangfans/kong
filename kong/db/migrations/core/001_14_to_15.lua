f3b473c5f43916b8616d7b50fb75a0bc4723c3fd
Enrique García Cota <kikito@gmail.com>
2018-10-29 15:11:30 +0100

    feat(plugins) add plugins.run_on field migration

--- kong/db/migrations/core/001_14_to_15.lua
+++ kong/db/migrations/core/001_14_to_15.lua
@@ -123,6 +123,17 @@ return {
       END;
       $$;
 
+      DO $$
+      BEGIN
+        ALTER TABLE IF EXISTS ONLY "plugins" ADD "run_on" TEXT UNIQUE;
+      EXCEPTION WHEN DUPLICATE_COLUMN THEN
+        -- Do nothing, accept existing state
+      END;
+      $$;
+
+      CREATE INDEX IF NOT EXISTS "plugins_run_on_idx" ON "plugins" ("run_on");
+
+      UPDATE plugins SET run_on = "gateway" WHERE run_on IS NULL;
 
 
       ALTER TABLE IF EXISTS ONLY "apis"
@@ -190,7 +201,6 @@ return {
       CREATE INDEX IF NOT EXISTS routes_name_idx ON routes(name);
 
 
-
       CREATE TABLE IF NOT EXISTS plugins_temp(
         id uuid,
         created_at timestamp,
@@ -198,6 +208,7 @@ return {
         route_id uuid,
         service_id uuid,
         consumer_id uuid,
+        run_on text,
         name text,
         config text, -- serialized plugin configuration
         enabled boolean,
@@ -241,6 +252,7 @@ return {
           route_id = "uuid",
           service_id = "uuid",
           consumer_id = "uuid",
+          run_on = "text",
           created_at = "timestamp",
           enabled = "boolean",
           cache_key = "text",
@@ -258,6 +270,7 @@ return {
         consumer_id = "consumer_id",
         created_at = "created_at",
         enabled = "enabled",
+        run_on = function(row) return "gateway" end,
         cache_key = function(row)
           return table.concat({
             "plugins",
@@ -285,6 +298,7 @@ return {
           route_id uuid,
           service_id uuid,
           consumer_id uuid,
+          run_on text,
           name text,
           config text, -- serialized plugin configuration
           enabled boolean,
@@ -298,6 +312,7 @@ return {
         CREATE INDEX IF NOT EXISTS ON plugins(service_id);
         CREATE INDEX IF NOT EXISTS ON plugins(consumer_id);
         CREATE INDEX IF NOT EXISTS ON plugins(cache_key);
+        CREATE INDEX IF NOT EXISTS ON plugins(run_on);
       ]]))
 
       plugins_def = {
@@ -310,6 +325,7 @@ return {
           route_id = "uuid",
           service_id = "uuid",
           consumer_id = "uuid",
+          run_on = "text",
           created_at = "timestamp",
           enabled = "boolean",
           cache_key = "text",
@@ -325,6 +341,7 @@ return {
         route_id = "route_id",
         service_id = "service_id",
         consumer_id = "consumer_id",
+        run_on = "run_on",
         created_at = "created_at",
         enabled = "enabled",
         cache_key = "cache_key",
