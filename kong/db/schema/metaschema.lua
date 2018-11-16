40ec5255107ce490ba38d4d14fa0c9f7c08a38d1
Enrique García Cota <kikito@gmail.com>
2018-11-16 20:11:49 +0100

    feat(schema) allow nullable on top-level schema definitions
    This is to allow typedefs with nullable = false inside.

--- kong/db/schema/metaschema.lua
+++ kong/db/schema/metaschema.lua
@@ -66,6 +66,7 @@ local field_schema = {
   { abstract = { type = "boolean" }, },
   { generate_admin_api = { type = "boolean" }, },
   { legacy = { type = "boolean" }, },
+  { nullable = { type = "boolean" }, },
 }
 
 for _, field in ipairs(validators) do
