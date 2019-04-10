ebedb31af1f6bdfe458e24eaa60c2c48c3106b6c
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-04-10 10:01:49 +0300

    perf(runloop) only check plugin combinations in use (ports #4494 over)
    Thank you @hishamhm!

--- kong/runloop/handler.lua
+++ kong/runloop/handler.lua
@@ -797,6 +797,7 @@ do
     local new_plugins = {
       map = {},
       cache = {},
+      combos = {}
     }
 
     local counter = 0
@@ -831,6 +832,12 @@ do
 
         local cache_key = kong.db.plugins:cache_key(plugin)
         new_plugins.cache[cache_key] = plugin
+
+        local combo_key = (plugin.route    and 1 or 0)
+                        + (plugin.service  and 2 or 0)
+                        + (plugin.consumer and 4 or 0)
+
+        new_plugins.combos[combo_key] = true
       end
 
       counter = counter + 1
